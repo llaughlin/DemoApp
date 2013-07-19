46a0a3107ef02f4edbb136d0ae3056d66c862b84
Logan Laughlin <loganlaughlin@gmail.com>
2013-07-19 15:23:17 -0500

    Added coffeescript bundle.

--- DemoApp/App_Start/BundleConfig.cs
+++ DemoApp/App_Start/BundleConfig.cs
@@ -24,6 +24,9 @@ namespace DemoApp
             bundles.Add(new ScriptBundle("~/bundles/ajaxlogin").Include(
                 "~/Scripts/app/ajaxlogin.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/github/CoffeeScript1.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
