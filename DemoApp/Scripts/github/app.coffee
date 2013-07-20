44f70c699d3c0910a0811a7d0d963ed98d1e7621
Logan Laughlin <loganlaughlin@gmail.com>
2013-07-20 18:54:06 -0500

    Cleaned up mappings so they don't use $.extend

--- DemoApp/Scripts/github/app.coffee
+++ DemoApp/Scripts/github/app.coffee
@@ -1,50 +1,54 @@
+commitModel = (data) ->
+    ko.mapping.fromJS(data, {}, this)
 
+    @files = ko.observableArray()
+    return
+
+fileModel = (data) ->
+    ko.mapping.fromJS(data, {}, this)
+
+    @badgeStatus = ko.computed(->
+        switch @status()
+            when 'added' then 'badge badge-success'
+            when 'modified' then 'badge badge-info'
+            when 'removed' then 'badge badge-important'
+            else 'badge'
+    , this)
+    return
 
 ViewModel = -> 
     self = this
     self.repoName = ko.observable("llaughlin/demoapp")
     self.commits = ko.observableArray()
-    ###[
-            {commit:{message: "First Commit"}, 
-            committer:{login: "llaughlin"}, sha: ko.observable("123")}
-    ]###
-    #)
-    
     
     self.getCommits = ->
-        #console.log("Getting commits")
         url = "https://api.github.com/repos/" + self.repoName() + "/commits"
-       # mapping = 'commit': {create: -> (new -> this.files = ko.observableArray())}
         $.get(url)
         .done((data) =>
-            #console.log(data)
-            $.each(data, (i, elem) ->
-                $.extend(elem.commit, {files: ko.observableArray()})
-            )
-            ko.mapping.fromJS(data, {}, self.commits)
+            mapping = {
+                'commit' : {
+                    create: (options) -> new commitModel(options.data)}
+            }
+
+            ko.mapping.fromJS(data, mapping, self.commits)
         )
-        #ko.mapping.fromJS(window.commitData, {}, self.commits)
 
     self.getFiles = (commit) -> 
-        #console.log(commit)
         if not commit.commit.files.length
             url = "https://api.github.com/repos/" + self.repoName() + "/commits/" + commit.sha()
-            ###$.get(url)
-            .done((data) =>
-                console.log("GitHub data", data)
-                ko.mapping.fromJS(data, {}, commit.commit)
-                console.log("KO Commit", ko.toJS(commit.commit))
-            )###
-            console.log("GitHub data", window.fileData)
-            ko.mapping.fromJS(window.fileData, {}, commit.commit)
-            console.log("KO Commit", commit.commit)
+            $.get(url)
+            .done((data) ->
+                mapping = {
+                    'files': {
+                        create: (options) -> new fileModel(options.data)
+                    }
+                }
+
+                ko.mapping.fromJS(data, mapping, commit.commit)
+            )
     return @
  
-
 $( ->
-
     window.viewModel = new ViewModel()
     ko.applyBindings(viewModel)
-
-    
 )
--- DemoApp/Scripts/github/app.js
+++ DemoApp/Scripts/github/app.js
@@ -1,45 +1,63 @@
 ﻿(function() {
-  var ViewModel;
+  var ViewModel, commitModel, fileModel;
+
+  commitModel = function(data) {
+    ko.mapping.fromJS(data, {}, this);
+    this.files = ko.observableArray();
+  };
+
+  fileModel = function(data) {
+    ko.mapping.fromJS(data, {}, this);
+    this.badgeStatus = ko.computed(function() {
+      switch (this.status()) {
+        case 'added':
+          return 'badge badge-success';
+        case 'modified':
+          return 'badge badge-info';
+        case 'removed':
+          return 'badge badge-important';
+        default:
+          return 'badge';
+      }
+    }, this);
+  };
 
   ViewModel = function() {
     var self;
     self = this;
     self.repoName = ko.observable("llaughlin/demoapp");
     self.commits = ko.observableArray();
-    /*[
-            {commit:{message: "First Commit"}, 
-            committer:{login: "llaughlin"}, sha: ko.observable("123")}
-    ]
-    */
-
     self.getCommits = function() {
       var url,
         _this = this;
       url = "https://api.github.com/repos/" + self.repoName() + "/commits";
       return $.get(url).done(function(data) {
-        $.each(data, function(i, elem) {
-          return $.extend(elem.commit, {
-            files: ko.observableArray()
-          });
-        });
-        return ko.mapping.fromJS(data, {}, self.commits);
+        var mapping;
+        mapping = {
+          'commit': {
+            create: function(options) {
+              return new commitModel(options.data);
+            }
+          }
+        };
+        return ko.mapping.fromJS(data, mapping, self.commits);
       });
     };
     self.getFiles = function(commit) {
       var url;
       if (!commit.commit.files.length) {
         url = "https://api.github.com/repos/" + self.repoName() + "/commits/" + commit.sha();
-        /*$.get(url)
-        .done((data) =>
-            console.log("GitHub data", data)
-            ko.mapping.fromJS(data, {}, commit.commit)
-            console.log("KO Commit", ko.toJS(commit.commit))
-        )
-        */
-
-        console.log("GitHub data", window.fileData);
-        ko.mapping.fromJS(window.fileData, {}, commit.commit);
-        return console.log("KO Commit", commit.commit);
+        return $.get(url).done(function(data) {
+          var mapping;
+          mapping = {
+            'files': {
+              create: function(options) {
+                return new fileModel(options.data);
+              }
+            }
+          };
+          return ko.mapping.fromJS(data, mapping, commit.commit);
+        });
       }
     };
     return this;
